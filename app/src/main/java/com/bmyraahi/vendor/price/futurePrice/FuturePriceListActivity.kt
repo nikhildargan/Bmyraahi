48da31c02c9aa5962e2f1296bfd34bb1a7e3809e
sunwicked <sunnykraditya@gmail.com>
2018-09-03 08:59:29 +0530

    Crash Fixed

--- app/src/main/java/com/bmyraahi/vendor/price/futurePrice/FuturePriceListActivity.kt
+++ app/src/main/java/com/bmyraahi/vendor/price/futurePrice/FuturePriceListActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bmyraahi.vendor.R
 import com.bmyraahi.vendor.price.Price
+import com.bmyraahi.vendor.price.RowType
 import kotlinx.android.synthetic.main.activity_future_price.*
 
 class FuturePriceListActivity : AppCompatActivity() {
@@ -14,6 +15,8 @@ class FuturePriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_future_price)
+        items.add(Price(RowType.PRICE,800))
+        items.add(Price(RowType.NO_PRICE,800))
         rvFuturePrice.adapter = FuturePriceAdapter(items)
 
         btnFuturePrice.setOnClickListener {
